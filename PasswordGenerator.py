c9dd69f25a08c1179d3eb9f311a34a52ed226422
Ryan Sanchez <40903678+ryan-31@users.noreply.github.com>
2021-09-26 17:30:26 -0400

    Version 3.0 - Password Customization, Improved GUI
    Fully utilized random(), added password customization, added entry widget and radio buttons

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,43 +1,72 @@
-# Password Generator
+# --------------------------------------------------------------------------------------------------
+# Created by Ryan S (GitHub: ryan-31)
+# Updated 9/26/21
+# Fully utilized random(), added password customization, added entry widget and radio buttons
+# Version 3.0
+# ---------------------------------------------------------------------------------------------------
+
 import random
 import string
 import tkinter
-mainWin = tkinter.Tk()
-
-mainWin.title("Password Generator")
-txtLbl1 = tkinter.Label(mainWin, text = "Hello. This program will generate a random and secure up to 35 character password using letters, numbers, capitalization, and special characters.", font = ("Times New Roman", 14))
-txtLbl2 = tkinter.Label(mainWin, text = "If the password generated is too long, simply use just a portion as your passcode. If it is too short, simply run the program agian and generate a second password to combine with the first.", font = ("Times New Roman", 14))
-txtLbl3 = tkinter.Label(mainWin, text = "This password generator does not save/store passwords created.", font = ("Times New Roman", 14))
-txtLbl1.pack()
-txtLbl2.pack()
-txtLbl3.pack()
+from tkinter import *
 
 
-from tkinter import Text
+def create_pass():
+    password = ""
 
-pswrd = Text(mainWin,height = 1)
+    length = int(passLength.get())
+    symbols = sym.get()
 
-def generate():
-    pswrd = Text(mainWin,height = 1)
-    # clearing previous password
-    password = " "
-    specialChar = ['!', '@', '#', '$', '%','^','&','*', '(', ')','-','_', '+','=','|','|','?','>','<','/','.',',']
-    animals = ['DIng0', 'Mo0se', 'mOusE', 'sHeEP', 'zEbRa', 'sKuNk', 'BiSoN', 'wHAle', 'KoALa', 'LemUR', 'hYEnA', 'CaMEl','cHImP', 'RhinO' ]
-    #initialize password with a random animal
-    password = random.choice(animals)
-    for i in range(0, 9):
-        password = password + str(random.randint(0,9))
-        password = password + random.choice(string.ascii_letters)
-        password = password + random.choice(specialChar)
-    pswrd.insert(1.0, password)
-    pswrd.pack()
-    pswrd.configure(state = "disabled")
-    pswrd.configure(bg=mainWin.cget('bg'), relief="flat")
-        
-from tkinter import Button
-b = Button(mainWin, text ="Generate", command=generate)
-b.pack()
+    if not symbols:
+        for i in range(length):
+            password = password + random.choice(string.ascii_letters + string.digits)
+    else:
+        for i in range(length):
+            password = password + random.choice(string.ascii_letters + string.digits + string.punctuation)
+
+    generated_pass.config(text=password)
+    generated_pass.config(font=('helvetica', 16))
+    generated_pass.pack()
 
 
+root = Tk()
+root.title = "Password Generator"
+root.geometry('800x600')
 
-mainWin.mainloop()
+titleLabel = Label(root, text="Password Generator")
+titleLabel.config(font=('helvetica', 20))
+titleLabel.pack()
+
+subTitle = Label(root, text='A secure password is very important\nin keeping your accounts safe from hackers and cyber'
+                            'criminals!\n\n')
+subTitle.config(font=('consolas', 12))
+subTitle.pack()
+
+lengthLabel = Label(root, text='Enter your desired password length:')
+lengthLabel.config(font=('helvetica', 14))
+passLength = StringVar()
+lengthEntry = tkinter.Entry(root, textvariable=passLength)
+lengthLabel.pack()
+lengthEntry.pack()
+
+symbolsLabel = Label(root, text='Would you like to include symbols?')
+symbolsLabel.config(font=('helvetica', 14))
+sym = tkinter.BooleanVar()
+symbolsRadio = Radiobutton(root, text='Yes', variable=sym, value=True)
+symbolsRadio.config(font=('helvetica', 14))
+
+symbolsRadio2 = Radiobutton(root, text='No', variable=sym, value=False)
+symbolsRadio2.config(font=('helvetica', 14))
+
+symbolsLabel.pack()
+symbolsRadio.pack()
+symbolsRadio2.pack()
+
+# Generate Password Button
+generate = Button(root, text='Generate Password', command=create_pass)
+generate.pack()
+
+generated_pass = Label(root, text='')
+
+
+root.mainloop()
